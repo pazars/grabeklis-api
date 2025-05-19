388f78fffd6c6ce07e9818c1c2d419e59759b8ae
davispazars <davis.pazars@gmail.com>
2025-05-19 21:30:33 +0300

    Basic Gemini call

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
 from settings import settings
 from datetime import datetime
 from fastapi.responses import JSONResponse
+from typing import Literal
+from google import genai
+from google.genai import types
 
 
 collections: dict[str, AsyncIOMotorCollection] = {
@@ -14,7 +17,7 @@ collections: dict[str, AsyncIOMotorCollection] = {
 category_mapping: dict[str, str | None] = {
     "c0": None,
     "c1": "Latvijā",
-    "c2": "Pasaulē", 
+    "c2": "Pasaulē",
 }
 
 
@@ -52,9 +55,13 @@ def parse_date(date: str) -> datetime:
         raise ValueError("Invalid date format. Use YYYYMMDD.")
 
 
-async def fetch_titles(filter_date: datetime, category: str = None) -> list[str]:
+async def fetch_article(
+    filter_date: datetime,
+    article_part: Literal["title", "summary", "article"],
+    category: str = None,
+) -> list[str]:
     """
-    Fetches titles from MongoDB filtered by date and optionally by category.
+    Fetches chosen article part from MongoDB filtered by date and optionally by category.
     """
     col = collections["collection"]
 
@@ -69,10 +76,28 @@ async def fetch_titles(filter_date: datetime, category: str = None) -> list[str]
         query["category"] = category
 
     # Query MongoDB
-    posts = await col.find(query, {"_id": 0, "title": 1}).to_list(length=100)
+    posts = await col.find(query, {"_id": 0, article_part: 1}).to_list(length=100)
 
     # Extract titles
-    return [entry["title"] for entry in posts]
+    return [entry[article_part] for entry in posts]
+
+
+@app.get("/articles/{date}")
+async def get_articles_by_date(date: str = Path(..., regex=r"^\d{8}$")) -> JSONResponse:
+    """
+    Filters posts by a specific date (YYYYMMDD) and returns their articles.
+    """
+    try:
+        filter_date = parse_date(date)
+    except ValueError as e:
+        return JSONResponse(content={"error": str(e)}, status_code=400)
+
+    articles = await fetch_article(filter_date, "article")
+    logger.debug(f"Found {len(articles)} posts for date {date}")
+
+    return JSONResponse(
+        content={"articles": articles}, media_type="application/json; charset=utf-8"
+    )
 
 
 @app.get("/titles/{date}")
@@ -85,10 +110,12 @@ async def get_titles_by_date(date: str = Path(..., regex=r"^\d{8}$")) -> JSONRes
     except ValueError as e:
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
-    titles = await fetch_titles(filter_date)
+    titles = await fetch_article(filter_date, "title")
     logger.debug(f"Found {len(titles)} posts for date {date}")
 
-    return JSONResponse(content={"titles": titles}, media_type="application/json; charset=utf-8")
+    return JSONResponse(
+        content={"titles": titles}, media_type="application/json; charset=utf-8"
+    )
 
 
 @app.get("/titles/{category_key}/{date}")
@@ -104,7 +131,57 @@ async def get_titles_by_category_and_date(
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
     category = category_mapping.get(category_key)
-    titles = await fetch_titles(filter_date, category)
+    titles = await fetch_article(filter_date, "title", category)
     logger.debug(f"Found {len(titles)} posts for category '{category}' and date {date}")
 
-    return JSONResponse(content={"titles": titles}, media_type="application/json; charset=utf-8")
+    return JSONResponse(
+        content={"titles": titles}, media_type="application/json; charset=utf-8"
+    )
+
+
+@app.get("/summarise/{date}")
+async def summarise_articles_for_date(
+    date: str = Path(..., regex=r"^\d{8}$"),
+) -> JSONResponse:
+    client = genai.Client(
+        vertexai=True,
+        project=settings.GCP_PROJECT_ID,
+        location=settings.VERTEXAI_REGION,
+    )
+
+    dt = parse_date(date)
+    db_response = await fetch_article(dt, "article")
+    articles = types.Part.from_text(text=" ".join(db_response))
+
+    model = "gemini-2.0-flash-001"
+    contents = [
+        types.Content(role="user", parts=[articles]),
+    ]
+
+    generate_content_config = types.GenerateContentConfig(
+        temperature=1,
+        top_p=1,
+        max_output_tokens=8192,
+        safety_settings=[
+            types.SafetySetting(category="HARM_CATEGORY_HATE_SPEECH", threshold="OFF"),
+            types.SafetySetting(
+                category="HARM_CATEGORY_DANGEROUS_CONTENT", threshold="OFF"
+            ),
+            types.SafetySetting(
+                category="HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold="OFF"
+            ),
+            types.SafetySetting(category="HARM_CATEGORY_HARASSMENT", threshold="OFF"),
+        ],
+        system_instruction=[types.Part.from_text(text=settings.SUMMARY_SYSTEM_PROMPT)],
+    )
+
+    response = client.models.generate_content(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    )
+
+    return JSONResponse(
+        content={"summary": response.text},
+        media_type="application/json; charset=utf-8",
+    )
--- settings.py
+++ settings.py
@@ -2,9 +2,16 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
+    # MongoDB
     MONGO_URI: str
     MONGO_DB: str
     MONGO_COLLECTION: str
+
+    # Google Cloud Platform (GCP)
+    GCP_PROJECT_ID: str
+    VERTEXAI_REGION: str
+
+    SUMMARY_SYSTEM_PROMPT: str
 
     class Config:
         # Load .env file in development mode
